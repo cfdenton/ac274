a0a2758d8f8d29758735a070e4bdd6bb5da398fc
Carl Denton <carldenton@college.harvard.edu>
2016-09-26 14:39:19 -0400

    homework02

--- continuity.py
+++ continuity.py
@@ -2,21 +2,25 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+
 import argparse
 import math
 
-dx = 1.0 
-dt = 0.1 
-nsteps = 800
-N = 500
+matplotlib.use('MacOSX')
+
 var = .1
 
-
+# params have form [dx, dt]
 ### state calculations ###
-def upwind_sim_1D(initial_state, u):
-    print("This is the 1D upwind simulation!")
-    N = initial_state.size
-
+def upwind_sim_1D(params, initial_state, u):
+    #print("This is the 1D upwind simulation!")
+    N = params[0] 
+    nsteps = params[1]
+    dx = params[2]
+    dt = params[3]
     # make transport matrix
     transport_matrix = np.zeros((N, N), dtype=np.float32)
     for i in range(N):
@@ -26,7 +30,7 @@ def upwind_sim_1D(initial_state, u):
         else:
             transport_matrix[i][i] = 1 + u[i]*dt/dx
             transport_matrix[i][(i+1)%N] = -u[(i+1)%N]*dt/dx
-    check_stability(transport_matrix)
+    # check_stability(transport_matrix)
  
     # compute states
     states = [] 
@@ -37,9 +41,11 @@ def upwind_sim_1D(initial_state, u):
         states.append(state)
     return states
     
-def shasta_sim_1D(initial_state, u):
-    print("This is the 1D SHASTA simulation.")
-    N = initial_state.size
+def shasta_sim_1D(params, initial_state, u):
+    N = params[0] 
+    nsteps = params[1]
+    dx = params[2]
+    dt = params[3]
 
     states = []
     states.append(initial_state)
@@ -47,19 +53,6 @@ def shasta_sim_1D(initial_state, u):
     next_state = np.empty(state.shape)
     
     anti_dif = np.empty(state.shape)
-    # build transport matrix
-    transport = np.empty((N, N))
-    for i in range(N):
-        q_plus = (.5 - u[i]*dt/dx)/(1 + (u[(i+1)%N] - u[i])*dt/dx)
-        q_minus = (.5 + u[i]*dt/dx)/(1 - (u[(i-1)%N] - u[i])*dt/dx)
-        next_state[i] = .5 * q_minus * q_minus * (state[(i-1)%N] - state[i]) \
-                      + .5 * q_plus * q_plus * (state[(i+1)%N] - state[i]) \
-                      + (q_minus + q_plus)*state[i]
-        transport[i][(i-1)%N] = .5 * q_minus * q_minus
-        transport[i][i] = -.5 * q_minus * q_minus - .5 * q_plus * q_plus  
-        transport[i][(i+1)%N] = .5 * q_plus * q_plus
-    check_stability(transport)
-
 
     for i in range(nsteps):
         sys.stdout.write("\rCompute state " + str(i))
@@ -91,11 +84,10 @@ def shasta_sim_1D(initial_state, u):
         next_state = np.add(next_state, anti_dif)
         state = np.copy(next_state)
         states.append(state)
-        #print(state)
     sys.stdout.write('\n')
     return states
 
-def fpe_sim_1D(initial_state, u, d):
+def fpe_sim_1D(params, initial_state, u, d):
     print("This is the 1D Fokker-Planck simulation.")
     N = initial_state.size
 
@@ -121,26 +113,33 @@ def fpe_sim_1D(initial_state, u, d):
 
 
 ### initial conditions ###
-def make_gaussian():
-    x = np.linspace(-1.0, +1.0, N)
+def make_gaussian(nodes):
+    x = np.linspace(-1.0, +1.0, nodes)
     return .5*np.exp(-x ** 2/(2 * var * var))
 
-def make_box():
-    x = np.zeros(N)
-    x[math.floor(3*N/8):math.floor(5*N/8)].fill(1)
+def make_box(nodes):
+    x = np.zeros(nodes)
+    x[math.floor(3*nodes/8):math.floor(5*nodes/8)].fill(1)
     return x
 
-k = 2
-def make_quadratic():
-    x = np.linspace(-1.0, +1.0, N)
+def make_uniform(nodes, u):
+    return np.full(nodes, u)
+
+def make_quadratic(nodes):
+    x = np.linspace(-1.0, +1.0, nodes)
     return -k * x 
 
-def make_bistable():
-    x = np.linspace(-1.0, +1.0, N)
+def make_bistable(nodes, wind):
+    x = np.linspace(-1.0, +1.0, nodes)
     u = -k*x*(1 - 4*x*x) + k*x*x*x 
     plt.plot(x, -k*(x*4)*x*(1 - (x*4)*x))
     plt.plot(x, u)
     return u
+
+def make_2D_ex(nodes):
+    l = np.linspace(-1.0, +1.0, nodes)
+    (x, y) = np.meshgrid(l, l)
+    return x, y, np.array(np.exp(- x**2 - y**2))
 
 
 ### checker functions ###
@@ -155,23 +154,29 @@ def check_stability(transport):
             + str(np.absolute(eig_values).max()))
 
 def check_convergence(sim_func, initial_func, u_func):
-    for n in arange(10, 1000):
-        N = n
-        dx = 10/N 
-        states1 = sim_func(initial_func(), u_func())
-        N = 2*n
-        dx = 10/N
-        states2 = sim_func(initial_func(), u_func())
-        
+    start, end = 100, 2000 
+    difs = np.empty(end-start)            
+    for n in range(start, end):
+        sys.stdout.write("\rChecking N = " + str(n))
+        dx = .1 
+        params = [n, 105, dx, dx] 
+        states1 = sim_func(params, initial_func(params[0]), u_func(params[0], .1))
+
+        params = [2*n, 105, dx/2, dx]
+        states2 = sim_func(params, initial_func(params[0]), u_func(params[0], .1))
+
+        dif_state = np.zeros(n)
+        for i in range(n):
+            dif_state[i] = states1[100][i] - states2[100][2*i]    
+        difs[n - start] = np.sqrt(np.sum((dif_state ** 2)/n))
+    return difs
          
         
-
-
 ### animations ###    
 def animate_states_1D(states):
     fig, ax = plt.subplots()
-    	
-    x = np.linspace(-1.0, +1.0, N)
+    nodes = states[0].shape[0]
+    x = np.linspace(-1.0, +1.0, nodes)
     line, = ax.plot(x, states[0])
     ax.set_ylim([0, 2])
 
@@ -183,28 +188,29 @@ def animate_states_1D(states):
         line.set_ydata(np.ma.array(x, mask=True))
         return line,
 
-    ani = animation.FuncAnimation(fig, animate, np.arange(1, nsteps), init_func=init,
+    ani = animation.FuncAnimation(fig, animate, np.arange(1, len(states)), init_func=init,
                                   interval=25, blit=False)
     plt.show()
+
+'''
+def animate_states_2D(states):
+    # check out that stack exchange answer 
+'''
     
+
 
 ### main ###
 def main(arguments):
     parser = argparse.ArgumentParser()
-    
-    print('Hello world!')
-    wind = np.linspace(0, -10, N)
-    wind.fill(5)
-    #wind = np.exp(-wind*wind/(2*var*var))
-    #states = shasta_sim_1D(make_box(), wind)
-    states = upwind_sim_1D(make_box(), wind)
-    u = make_bistable()
-    #dif = np.linspace(-1.0, +1.0, N)
-    #dif = np.exp(-dif*dif/(2*var*var))
-    #dif.fill(1)
-    #states = shasta_sim_1D(make_box(), u)
-    animate_states_1D(states)
-    
+    x, y, z = make_2D_ex(10)
+    print(x.shape)
+    print(y.shape)
+    print(z.shape)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    plt.show()
+
 
 
 if __name__ == '__main__':
